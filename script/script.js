af1269a2e1f5fd0a36d1a0176496abfe23ddb693
Satria <satriaardiansyah133@gmail.com>
2024-02-04 20:58:07 +0700

    first commit

---
+++ script/script.js
@@ -0,0 +1,8 @@
+/*=============== ADD BLUR TO HEADER ===============*/
+const blurHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("blur-header")
+    : header.classList.remove("blur-header");
+};
+window.addEventListener("scroll", blurHeader);
